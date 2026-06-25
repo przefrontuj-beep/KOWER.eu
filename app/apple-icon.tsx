30dd441de92982452849ecd9e06aafc34d0c4a8f
DamianSulima <damian.k.sulima@gmail.com>
2026-06-25 22:44:14 +0200

    add dynamic favicon and apple icon

---
+++ app/apple-icon.tsx
@@ -0,0 +1,34 @@
+import { ImageResponse } from 'next/og';
+
+export const runtime = 'edge';
+
+export const size = {
+  width: 180,
+  height: 180,
+};
+export const contentType = 'image/png';
+
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 110,
+          background: '#487330',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          color: 'white',
+          borderRadius: '24px',
+          fontFamily: 'serif',
+          fontWeight: 600,
+        }}
+      >
+        K
+      </div>
+    ),
+    { ...size }
+  );
+}
